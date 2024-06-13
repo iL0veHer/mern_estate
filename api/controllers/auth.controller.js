57ed0082ff614d7e664da71eb2b155490477e6d8
RavisherSinghGill <gill1122ravi@gmail.com>
2024-06-13 00:18:09 -0400

    create a middlware and fn to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res,next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 12);
 
@@ -18,6 +18,6 @@ export const signup = async (req, res) => {
             newUser,
         });
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error)
     }
 };
