2fcb6fef8d392b0ac5c91b1a1ab7e66b94dca4f3
RavisherSinghGill <gill1122ravi@gmail.com>
2024-06-13 02:54:17 -0400

    complete sign up page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
    return (
-      <header className="bg-slate-200 shadow-md ">
+      <header className="bg-slate-200 shadow-md">
          <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
             <Link to="/">
                <h1 className="font-bold text-small sm:text-xl flex flex-wrap">
@@ -11,31 +11,43 @@ function Header() {
                   <span className="text-slate-700">Estate</span>
                </h1>
             </Link>
-            <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+            <form
+               className="bg-slate-100 p-3 rounded-lg flex items-center"
+               aria-label="Search form"
+            >
                <input
                   type="text"
                   placeholder="Search..."
                   className="bg-transparent focus:outline-none w-24 sm:w-64"
+                  aria-label="Search"
                />
                <FaSearch className="text-slate-600" />
             </form>
             <ul className="flex gap-4">
-               <Link to="/">
-                  <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+               <li className="hidden sm:inline">
+                  <Link
+                     to="/"
+                     className="text-slate-700 hover:underline cursor-pointer"
+                  >
                      Home
-                  </li>
-               </Link>
-               <Link to="/about">
-                  <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+                  </Link>
+               </li>
+               <li className="hidden sm:inline">
+                  <Link
+                     to="/about"
+                     className="text-slate-700 hover:underline cursor-pointer"
+                  >
                      About
-                  </li>
-               </Link>
-
-               <Link to="sign-up">
-                  <li className=" text-slate-700 hover:underline cursor-pointer">
+                  </Link>
+               </li>
+               <li>
+                  <Link
+                     to="/sign-in"
+                     className="text-slate-700 hover:underline cursor-pointer"
+                  >
                      Sign in
-                  </li>
-               </Link>
+                  </Link>
+               </li>
             </ul>
          </div>
       </header>
