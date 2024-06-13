377feea338dcde1e6d5369818e2abd96110c1b9c
RavisherSinghGill <gill1122ravi@gmail.com>
2024-06-12 23:01:33 -0400

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({});
+
+mongoose
+  .connect(process.env.MONGO)
+  .then((e) => {
+    console.log("connected to mongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(3000, () => {
-    console.log('guys, I am running. help!!!')
+  console.log('guys, I am running. help!!!');
 });
